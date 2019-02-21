ca7eccbef639e33353336f09d8b41cb861088fe5
ANUBHAVNATANI <anubhavnatani99@gmail.com>
2019-02-21 22:07:57 +0530

    improve insertion sort

--- Algorithms/Python/insertion_sort.py
+++ Algorithms/Python/insertion_sort.py
@@ -1,8 +1,13 @@
 def insertion_sort(arr):
     for i in range(1,len(arr)):
-        initial_element = arr[i]    
+        c = arr[i]
         j = i-1
-        while(j>=0 and key<) 
-
+        while(j>=0 and arr[j]>c):
+            arr[j+1] = arr[j]
+            j = j-1
+        arr[j+1] = c
+                
 
 a = [4,5,7,1,2,3,6,88,4,11,22,4,5]
+insertion_sort(a)
+print(a)
