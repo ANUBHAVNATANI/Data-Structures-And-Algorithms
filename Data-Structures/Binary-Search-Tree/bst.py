b27ad91fe13dc30872c024d6abd6a0f3455b01b7
ANUBHAVNATANI <anubhavnatani99@gmail.com>
2019-06-16 12:11:07 +0530

    more bst functions

--- Data-Structures/Binary-Search-Tree/bst.py
+++ Data-Structures/Binary-Search-Tree/bst.py
@@ -4,6 +4,7 @@ class Node:
         self.left = None
         self.right = None
         self.value = key
+        self.parent = None
 
 # insert function in bst
 
@@ -15,11 +16,13 @@ def insert(root, node):
         if root.value < node.value:
             if root.right is None:
                 root.right = node
+                node.parent = root
             else:
                 insert(root.right, node)
         else:
             if root.left is None:
                 root.left = node
+                node.parent = root
             else:
                 insert(root.left, node)
 
@@ -46,6 +49,112 @@ def postorder(root):
         postorder(root.right)
         print(root.value)
 
+# searching of node in bst
+
+
+def search(root, key):
+    if root is None or root.value == key:
+        return root
+    elif root.value < key:
+        return search(root.right, key)
+    else:
+        return search(root.left, key)
+
+
+#min and max in bst
+def minmum(root):
+    if root == None:
+        return None
+    while(root.left != None):
+        root = root.left
+    return root
+
+
+def maximum(root):
+    if root == None:
+        return None
+    while(root.right != None):
+        root = root.right
+    return root
+
+#sucessor and predessor in bst
+
+
+def treesucc(x):
+    if(x.right != None):
+        return minmum(x.right)
+    # to handle the case when there is no right sub tree
+    y = x.parent
+    while(y != None and x == y.right):
+        x = y
+        y = y.p
+    return y
+
+# predecessor code for bst
+
+
+def treepred(x):
+    if(x.left != None):
+        return maximum(x.left)
+    # to handle the case when there is no left sub tree
+    y = x.parent
+    while(y != None and x == y.left):
+        x = y
+        y = y.p
+    return y
+
+# different function for pred and successor
+
+
+def predsucc(root, key, pred, succ):
+    if(root == None):
+        return
+    elif(root.value == key):
+        pred = maximum(root.left)
+        succ = minmum(root.right)
+        return
+    elif(root.value < key):
+        pred = root
+        predsucc(root.right, key, pred, succ)
+    else:
+        succ = root
+        predsucc(root.left, key, pred, succ)
+
+# transplant procedure helps to replace the subtrees with in the tree
+
+
+def transplant(root, u, v):
+    # this procedure replaces the sub tree rooted at node u with the subtree rooted at node v
+    if u.parent == None:
+        # case when u is the root
+        root = v
+    elif u.parent.left == u:
+        u.parent.left = v
+    else:
+        u.parent.right = v
+    if v != None:
+        v.parent = u.parent
+
+# delete case in bst
+# at total we had to handle three cases for this sought of problem
+
+
+def delete(root, z):
+    # z is the node to delete
+    if z.left == None:
+        transplant(root, z, z.right)
+    elif z.right == None:
+        transplant(root, z, z.left)
+    else:
+        y = minmum(z.right)
+        if y.parent != z:
+            transplant(root, y, y.right)
+            y.right = z.right
+            y.right.parent = y
+        transplant(root, z, y)
+        y.left = z.left
+        y.left.p = y
+
 
 if __name__ == "__main__":
     r = Node(6)
@@ -60,3 +169,13 @@ if __name__ == "__main__":
     preorder(r)
     print("postorder")
     postorder(r)
+    print(search(r, 7).value)
+    print(search(r, 10))
+    print(minmum(r).value)
+    print(maximum(r).value)
+    a = search(r, 5)
+    v = treesucc(a)
+    print(v.value)
+    delete(r, a)
+    print("inorder")
+    inorder(r)
